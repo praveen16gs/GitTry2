8fc87f262f538a728a08c08f6a600068afebfede
praveen <praveen16.gs@gmail.com>
2021-03-06 11:47:47 +0530

    Initial Commit

---
+++ GitTry2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTry2
+//
+//  Created by Dhanasekaran on 06/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
